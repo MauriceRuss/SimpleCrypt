87c82f80731abb4228400c9906f2e255aa7d4d4c
Mo Russ <mauriceruss302@gmail.com>
2020-03-22 19:47:00 -0400

    encryptTextFile() wrote. 4 tests passed

--- Crypto/src/main/java/rocks/zipcodewilmington/ROT13.java
+++ Crypto/src/main/java/rocks/zipcodewilmington/ROT13.java
@@ -1,5 +1,7 @@
 package rocks.zipcodewilmington;
 
+import java.io.*;
+
 import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
@@ -10,25 +12,55 @@ public class ROT13  {
     }
 
     ROT13() {
+        this('a', 'm');
     }
 
 
     public String crypt(String text) throws UnsupportedOperationException {
+        for(int i = 0; i < text.length(); i++){
+            char c = text.charAt(i);
+            if(c >= 'a' && c <= 'm') c += 13;
+            else if (c >= 'A' && c <= 'M') c += 13;
+            else if (c >= 'n' && c <= 'z') c -= 13;
+            else if (c >= 'N' && c <= 'Z') c -= 13;
+        }
 
-        return "";
+        return text;
     }
 
     public String encrypt(String text) {
-        return text;
+        String encrypt = crypt(text);
+        return encrypt;
     }
 
     public String decrypt(String text) {
-        return text;
+        String decrypt = crypt(text);
+        return decrypt;
     }
 
     public static String rotate(String s, Character c) {
+        Integer index = s.indexOf(c);
+        return s.substring(index) + s.substring(0, index);
+    }
 
-        return "";
+    public static void encyptTextFile() throws Exception{
+        ROT13 rot13 = new ROT13();
+        File file = new File("/Users/mruss/Documents/Projects/SimpleCrypt/sonnet18.txt");
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String line = bufferedReader.readLine();
+            while (line != null){
+                stringBuilder.append(line).append("\n");
+                line = bufferedReader.readLine();
+            }
+            String fileString = stringBuilder.toString();
+            String fileEncrypted = rot13.encrypt(fileString);
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("/Users/mruss/Documents/Projects/SimpleCrypt/sonnet18.encrpted"));
+            bufferedWriter.write(fileEncrypted);
+            bufferedWriter.close();
+            System.out.println(fileEncrypted);
+
     }
 
 }
